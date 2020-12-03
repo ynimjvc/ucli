7ba2bf6e0d51348493af816556b3a0562bd9c7da
korelinvi <korelinse@gmail.com>
2020-12-04 01:42:40 +0300

    no message

--- src/index.js
+++ src/index.js
@@ -1,28 +1,55 @@
 const EventEmitter = require('events');
 const {version} = require('../package.json');
 
-const bus = new EventEmitter();
-bus.setMaxListeners(100);
+const app = () => {
 
-const plugins = new Map();
+    const plugins = new Map();
+    const bus = new EventEmitter();
+    const context = {
+        version: () => version,
+        plugins: name => name
+            ? [name, plugins.get(name)]
+            : Array.from(plugins.entries())
+    };
 
-const context = {
-    version: () => version(),
-    plugins: name => name
-        ? [name, plugins.get(name)]
-        : Array.from(plugins.entries())
+    bus.setMaxListeners(100);
+
+    const wrap = callback => flow => (...args) => callback(flow, ...args);
+
+    const use = (flow, callback) => {
+        callback(flow);
+
+        return flow;
+    };
+
+    const describe = (flow, name, description) => {
+        plugins.set(name, description);
+
+        return flow;
+    };
+
+    const on = (flow, name, callback) => {
+        bus.on(name, () => callback());
+
+        return flow;
+    };
+
+    const dispatch = (flow, name) => {
+        process.nextTick(() => bus.emit(name));
+
+        return flow;
+    };
+
+    const flow = {
+        context
+    };
+
+    flow.use = wrap(use)(flow);
+    flow.describe = wrap(describe)(flow);
+    flow.on = wrap(on)(flow);
+    flow.dispatch = wrap(dispatch)(flow);
+
+    return flow;
 };
 
-const on = (name, callback) => bus.on(name, () => callback(context));
-const dispatch = name => bus.emit(name);
-const plugin = (name, callback) => {
-    if (plugins.has(name)) {
-        throw new Error(`plugin ${name} allready registred`);
-    } else {
-        plugins.set(name, callback({on, dispatch}));
-    }
-};
-const app = () => process.nextTick(() => dispatch('start'));
-
-module.exports.plugin = plugin;
-module.exports.app = app;
+module.exports = app;
